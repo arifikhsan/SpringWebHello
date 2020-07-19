47510098b76cd2e0d6777e6447a530c529a2b39b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-19 16:20:15 +0700

    initial commit

---
+++ src/main/java/sh/now/arifikhsanudin/springwebhello/SpringWebHelloApplication.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.springwebhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/now/arifikhsanudin/springwebhello/SpringWebHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.springwebhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
